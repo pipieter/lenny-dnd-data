7ac45b22473ca043887ea352d5ac4e0c425e6c5a
DaFrankort <daniel.frankort@telenet.be>
2025-10-31 15:02:01 +0100

    Add basic vehicles.json parsing

--- src/vehicles.ts
+++ src/vehicles.ts
@@ -1,4 +1,5 @@
-import { Description } from './parser';
+import { Description, parseDescriptions } from './parser';
+import { getVehiclesUrl, getVehicleTokenUrl } from './urls';
 
 interface Vehicle {
     name: string;
@@ -91,4 +92,20 @@ interface ParsedVehicle {
     url: string;
     tokenUrl: string | null;
     description: Description[];
+}
+
+export function getVehicles(data: any): ParsedVehicle[] {
+    return (data.vehicle as Vehicle[]).map((v) => {
+        const url = getVehiclesUrl(v.name, v.source)
+        const tokenUrl = v.hasToken ? getVehicleTokenUrl(v.name, v.source) : null;
+        const description = v.entries ? parseDescriptions('', v.entries) : [];
+
+        return {
+            name: v.name,
+            source: v.source,
+            url: url,
+            tokenUrl: tokenUrl,
+            description: description
+        }
+    });
 }
