ed3b3b0429ea228c6b2b88b0125de9793d1d11f3
DaFrankort <daniel.frankort@telenet.be>
2025-10-31 14:20:17 +0100

    Add vehicles.ts with rudimentary interfaces

---
+++ src/vehicles.ts
@@ -0,0 +1,94 @@
+import { Description } from './parser';
+
+interface Vehicle {
+    name: string;
+    source: string;
+    page: number;
+    srd: boolean;
+    vehicleType: string;
+    size: string;
+    dimensions?: string[];
+    terrain: string[];
+    capCrew: number;
+    capPassenger: number;
+    capCargo?: number;
+    cost?: number;
+    pace?: number | object;
+    speed?: {
+        walk: number;
+        swim: number;
+        note: string;
+    };
+    ac?: number;
+    str?: number;
+    dex?: number;
+    con?: number;
+    int?: number;
+    wis?: number;
+    cha?: number;
+    hp?: number | object;
+    immune?: string[];
+    conditionImmune?: string[];
+    hull?: {
+        ac: number;
+        acFrom?: string[];
+        hp: number;
+        dt: number;
+    };
+    control?: {
+        name: string;
+        ac: number;
+        hp: number;
+        entries: string[];
+    }[];
+    movement: {
+        name: string;
+        ac: number;
+        hp: number;
+        hpNote: string;
+        speed: {
+            mode: string;
+            entries: string[];
+        }[];
+    };
+    weapon?: {
+        name: string;
+        crew?: number;
+        ac?: number;
+        hp?: number;
+        count?: number;
+        costs: object;
+        entries: string[];
+        action: {
+            name: string;
+            entries: string[];
+        }[];
+    }[];
+    actionThresholds: object;
+    action: (string | any)[];
+    trait: {
+        name: string;
+        entries: string[];
+    }[];
+    actionStation: {
+        name: string;
+        entries: string[];
+    }[];
+    reaction: {
+        name: string;
+        entries: string[];
+    }[];
+    entries: (string | any)[];
+    tokenCredit?: string;
+    hasToken: boolean;
+    hasFluff?: boolean;
+    hasFluffImages: boolean;
+}
+
+interface ParsedVehicle {
+    name: string;
+    source: string;
+    url: string;
+    tokenUrl: string | null;
+    description: Description[];
+}
