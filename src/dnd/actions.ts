4da9cbe4c88758f42d56102a8434f80e573b3b43
Pim Pieters <pim@pieters.tv>
2025-11-04 15:37:45 +0100

    Remove unused code

--- src/dnd/actions.ts
+++ src/dnd/actions.ts
@@ -2,7 +2,7 @@ import { Description, parseDescriptions, parseSingleTime } from '../parser';
 import { joinStringsWithOr } from '../util';
 
 import interfacesTI from '../interfaces-ti';
-import { CheckerT, createCheckers } from 'ts-interface-checker';
+import { createCheckers } from 'ts-interface-checker';
 import { validate } from '../validate';
 import { getActionsUrl } from '../urls';
 import { Action, Time } from '../interfaces';
@@ -31,36 +31,7 @@ function parseActionTime(times: Time[] | undefined): string {
     return joinStringsWithOr(results);
 }
 
-/*
-interface Action {
-    name: string;
-    source: string;
-    page: number;
-    srd: boolean;
-    basicRules: boolean;
-    time: any[];
-    entries: (string | any)[];
-}
-
-
-
 export function getActions(data: any): ParsedAction[] {
-    return (data.action as Action[]).map((action) => {
-        return {
-            name: action.name,
-            source: action.source,
-            url: getActionsUrl(action.name, action.source),
-            time: parseActionTime(action.time),
-            description: parseDescriptions('', action.entries),
-        };
-    });
-}
-*/
-
-export function getActions(data: any): ParsedAction[] {
-    console.log(Object.keys(data));
-    console.log(data.action);
-    console.log(ActionChecker);
     const actions = validate<Action>(data.action, ActionChecker);
     const parsed: ParsedAction[] = actions.map((action) => ({
         name: action.name,
@@ -69,6 +40,6 @@ export function getActions(data: any): ParsedAction[] {
         time: parseActionTime(action.time),
         description: parseDescriptions('', action.entries),
     }));
-    // TODO rewrite
+
     return parsed;
 }
