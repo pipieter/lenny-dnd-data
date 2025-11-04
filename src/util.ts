13bfbceb116471f451a134a8f937102fad8c8c4c
DaFrankort <daniel.frankort@telenet.be>
2025-11-04 14:13:18 +0100

    Removed unused import

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,6 @@
 import { title } from './parser';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import kleur = require('kleur');
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const commandExistsSync = require('command-exists').sync;
 
 export const BulletPoint = '\u2022'; // U+2022 •
 
