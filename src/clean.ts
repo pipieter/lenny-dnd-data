3809ffc98b539d1e69ce79976c4397e2985357a7
Pim Pieters <pim@pieters.tv>
2026-02-24 19:41:37 +0100

    Add missing h clean

--- src/clean.ts
+++ src/clean.ts
@@ -384,13 +384,16 @@ function hazard(text: string, _noFormat: boolean): string {
 }
 
 function hit(text: string, noFormat: boolean): string {
-    text = text.replaceAll(pattern('hit', 1), (_, p1) => {
-        if (!p1.startsWith('+') && !p1.startsWith('-')) {
-            // Add missing plus symbol in cases like {@hit 10} (should become +10)
-            p1 = '+' + p1;
+    // Add missing plus symbol in cases like {@hit 10} (should become +10)
+    function addSymbol(value: string) {
+        if (!value.startsWith('+') && !value.startsWith('-')) {
+            value = '+' + value;
         }
-        return p1;
-    });
+        return value;
+    }
+
+    text = text.replaceAll(pattern('hit', 1), (_, p1) => addSymbol(p1));
+    text = text.replaceAll(pattern('h', 1), (_, p1) => addSymbol(p1));
 
     if (noFormat) {
         text = text.replaceAll(pattern('h', 0), 'Hit: ');
