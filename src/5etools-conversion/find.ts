c20171076293e564c52f4b75b42619f82ab8906d
Pim Pieters <pim@pieters.tv>
2026-02-25 03:11:52 +0100

    Remove findEntry function

--- src/5etools-conversion/find.ts
+++
@@ -1,8 +0,0 @@
-export function findEntry(entries: any[], name: string, source: string): any | null {
-    for (const entry of entries) {
-        if (entry.name === name && entry.source === source) {
-            return entry;
-        }
-    }
-    return null;
-}
