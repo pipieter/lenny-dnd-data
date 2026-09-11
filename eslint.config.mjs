70a355914f8f15930c679594cafb95a3e3aa0a43
Pim Pieters <pim@pieters.tv>
2026-09-11 15:23:18 +0200

    Configure es-lint to ignore unused-vars errors if the variable starts with an underscore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,41 @@
 // eslint.config.js
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
 
-  {
-    plugins: {
-      'unused-imports': eslintPluginUnusedImports,
-    },
-    rules: {
-      'unused-imports/no-unused-imports': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
-      ],
-      'no-console': 'error',
+    {
+        plugins: {
+            'unused-imports': eslintPluginUnusedImports,
+        },
+        rules: {
+            'unused-imports/no-unused-imports': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    vars: 'all',
+                    args: 'after-used',
+                    ignoreRestSiblings: false,
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+            'no-console': 'error',
 
-      // IGNORE
-      'no-useless-escape': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+            // IGNORE
+            'no-useless-escape': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
     },
-  },
 
-  // Ignore console.log in util.ts
-  {
-    files: ['src/util.ts'],
-    rules: {
-      'no-console': 'off',
-    },
-  }
+    // Ignore console.log in util.ts
+    {
+        files: ['src/util.ts'],
+        rules: {
+            'no-console': 'off',
+        },
+    }
 );
