058877b0d58102501a32d06307d87abcb67c6d9d
ImEldie <daniel.frankort@telenet.be>
2026-09-13 11:32:42 +0200

    Add typing to names

--- src/data.ts
+++ src/data.ts
@@ -22,6 +22,7 @@ import {
 import { Language } from '../5etools-collector/types/language';
 import { LifeBackground, LifeClass } from '../5etools-collector/types/life';
 import { Monster } from '../5etools-collector/types/monster';
+import { SpeciesName } from '../5etools-collector/types/name';
 import { DNDObject } from '../5etools-collector/types/object';
 import { OptionalFeatures } from '../5etools-collector/types/optionalfeatures';
 import { Rule } from '../5etools-collector/types/rule';
@@ -32,7 +33,6 @@ import { Spell, SpellSource } from '../5etools-collector/types/spell';
 import { Status } from '../5etools-collector/types/status';
 import { TableGroup, TableTable } from '../5etools-collector/types/table';
 import { Vehicle, VehicleUpgrade } from '../5etools-collector/types/vehicle';
-import { SpeciesName } from './dnd/names';
 import { title } from './parser';
 import { existsSync, mkdirSync, readFileSync, readdirSync, writeFileSync } from 'fs';
 import { dirname, join } from 'path';
